e48afe831d56d6a11e6a79f1bdb5c23f9776435c
SNEHRANJAN28 <148560123+SNEHRANJAN28@users.noreply.github.com>
2026-05-30 15:56:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import string
 
 app = FastAPI(title="HEALTHHIVE API", version="1.0.0")
 
-df = pd.read_csv("patient_dataset_100k_updated.csv(1)")
+df = pd.read_csv("patient_dataset_100k_updated(1).csv")
 
 le_category = LabelEncoder()
 df["Category_Encoded"] = le_category.fit_transform(df["Category"])
